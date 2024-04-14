9c99c6df0f6a7a8775a9dbfb7b5c89672a0e95c0
Reumatyzm <jaroszewskakaja@gmail.com>
2024-04-14 19:31:28 +0200

    #14
    * Added the text info interactions system

--- Drug Office/Assets/Scripts/DialogueSrcipts/DialaogueTrigger.cs
+++ Drug Office/Assets/Scripts/DialogueSrcipts/DialaogueTrigger.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class DialaogueTrigger : MonoBehaviour
 {
     public Dialaogue dialaogue;
+    public DialaogueManagerScript dialaogueScript;
     [SerializeField] bool isRepeatable;
+    [SerializeField] bool isWantStop;
 
     public void OnTriggerEnter(Collider other)
     {
@@ -17,6 +19,20 @@ public class DialaogueTrigger : MonoBehaviour
             {
                 this.GetComponent<BoxCollider>().enabled = false;
             }
+
+            if (isWantStop == true)
+            {
+                PlayerMovement.playerSpeed = PlayerMovement.playerFreeze;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (isWantStop == false)
+        {
+            dialaogueScript.speechText.enabled = false;
+            dialaogueScript.npcNameText.enabled = false;
         }
     }
 
