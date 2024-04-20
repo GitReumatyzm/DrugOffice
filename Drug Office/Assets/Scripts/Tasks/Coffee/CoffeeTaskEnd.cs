fbb8c0b34322b873462a2c1a49128a3a83ab2daa
ALEKSANDERK1 <68990212+ALEKSANDERK1@users.noreply.github.com>
2024-04-20 15:51:01 +0200

    Coffee Task 1

---
+++ Drug Office/Assets/Scripts/Tasks/Coffee/CoffeeTaskEnd.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoffeeTaskEnd : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI coffeeTaskText;
+    [SerializeField] float time = 3f;
+    [SerializeField] GameObject CoffeeCupPlayer;
+    [SerializeField] GameObject CoffeeCupDesk1;
+    [SerializeField] GameObject CoffeeCupDesk2;
+
+    public void Interact()
+    {
+        coffeeTaskText.text = "<s>" + coffeeTaskText.text + "</s>";
+        StartCoroutine(destroyOnTime());
+        CoffeeCupPlayer.SetActive(false);
+        CoffeeCupDesk1.SetActive(true);
+        CoffeeCupDesk2.SetActive(true);
+    }
+
+    IEnumerator destroyOnTime()
+    {
+        yield return new WaitForSeconds(time);
+       coffeeTaskText.enabled = false;
+    }
+}
---
+++ Drug Office/Assets/Scripts/Tasks/Coffee/CoffeeTaskStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CoffeeTaskStart : MonoBehaviour
+{ 
+    [SerializeField] TextMeshProUGUI coffeeTaskText;
+    [SerializeField] GameObject CoffeeCup;
+
+    void Start()
+    {
+        Debug.Log("damn");
+    }
+
+    public void Interact()
+    {
+        coffeeTaskText.enabled = true;
+        Debug.Log("essa");
+        coffeeTaskText.text = $"Get this coffee to your bosses";
+        CoffeeCup.SetActive(true);
+    }
+}
