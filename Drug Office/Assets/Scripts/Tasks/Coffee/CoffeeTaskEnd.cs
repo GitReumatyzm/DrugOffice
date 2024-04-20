3e7fa8b32ff6d113db8337f35a69c2b36100661a
Reumatyzm <jaroszewskakaja@gmail.com>
2024-04-20 17:23:29 +0200

    #15.3
    * Minor fixes to coffe task

--- Drug Office/Assets/Scripts/Tasks/Coffee/CoffeeTaskEnd.cs
+++ Drug Office/Assets/Scripts/Tasks/Coffee/CoffeeTaskEnd.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class CoffeeTaskEnd : MonoBehaviour
+public class CoffeeTaskEnd : MonoBehaviour, iInteractable
 {
     [SerializeField] TextMeshProUGUI coffeeTaskText;
     [SerializeField] float time = 3f;
