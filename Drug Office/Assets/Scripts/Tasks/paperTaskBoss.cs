243bd617d6c54fcab1103891e76f299fe69e6fa0
Reumatyzm <jaroszewskakaja@gmail.com>
2024-04-21 22:47:32 +0200

    #16.1
    * Paper Task Finished

---
+++ Drug Office/Assets/Scripts/Tasks/paperTaskBoss.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperTaskBoss : MonoBehaviour, iInteractable
+{
+    public paperTaskEnd paperTaskEndScript;
+    [SerializeField] float time = 3f;
+    [SerializeField] GameObject docObj;
+    [SerializeField] List<GameObject> deskDocsObjs = new List<GameObject>();
+    [SerializeField] string taskEndMessage = "- Noice mate";
+
+    public void Interact()
+    {
+        if (gameObject.CompareTag("BossDeskTask") && paperTaskEndScript.isStorageRoomDone == true)
+        {
+            paperTaskEndScript.papersNumber = 0;
+            paperTaskEndScript.papersTaskText.text = "<s>" + taskEndMessage + $" ({paperTaskEndScript.papersNumber})" + "</s>";
+            docObj.SetActive(false);
+            foreach (GameObject obj in deskDocsObjs)
+            {
+                obj.SetActive(true);
+            }
+            StartCoroutine(destroyOnTime());
+        }
+    }
+
+    IEnumerator destroyOnTime()
+    {
+        yield return new WaitForSeconds(time);
+        paperTaskEndScript.papersTaskText.enabled = false;
+    }
+}
--- Drug Office/Assets/Scripts/Tasks/paperTaskEnd.cs
+++ Drug Office/Assets/Scripts/Tasks/paperTaskEnd.cs
@@ -5,12 +5,10 @@ using TMPro;
 
 public class paperTaskEnd : MonoBehaviour, iInteractable
 {
-    [SerializeField] TextMeshProUGUI papersTaskText;
-    [SerializeField] float time = 3f;
-    [SerializeField] GameObject docObj;
-    [SerializeField] List<GameObject> deskDocsObjs = new List<GameObject>();
+    public TextMeshProUGUI papersTaskText;
+    public string deskBossTaskText = "- Great work, fella! Now leave the rest on boss desk";
     public bool isStorageRoomDone;
-    private int papersNumber;
+    public int papersNumber;
 
     public void Interact()
     {
@@ -18,25 +16,7 @@ public class paperTaskEnd : MonoBehaviour, iInteractable
         {
           isStorageRoomDone = true;
           papersNumber = 2;
-          papersTaskText.text = $"- Great work, fella! Now leave the rest on boss desk ({papersNumber})";
+          papersTaskText.text = deskBossTaskText + $" ({papersNumber})";
         }
-        else if(gameObject.CompareTag("BossDeskTask") && isStorageRoomDone)
-        {
-            papersNumber = 0;
-            papersTaskText.text = $"- Noice mate ({papersNumber})";
-            papersTaskText.text = "<s>" + papersTaskText.text + "</s>";
-            docObj.SetActive(false);
-            foreach(GameObject obj in deskDocsObjs)
-            {
-                obj.SetActive(true);
-            }
-            StartCoroutine(destroyOnTime());
-        }
-    }
-
-    IEnumerator destroyOnTime()
-    {
-        yield return new WaitForSeconds(time);
-       papersTaskText.enabled = false;
     }
 }
