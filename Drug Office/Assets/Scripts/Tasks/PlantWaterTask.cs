3f134ef98e53b609e555313196cdfd4cd7ebe30a
Reumatyzm <jaroszewskakaja@gmail.com>
2024-04-23 20:04:38 +0200

    #17
    * Added plant task

---
+++ Drug Office/Assets/Scripts/Tasks/PlantWaterTask.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlantWaterTask : MonoBehaviour, iInteractable
+{
+    [SerializeField] TextMeshProUGUI waterPlantTaskText;
+    [SerializeField] string EndTaskMessage = "*sounds of draining water*";
+    [SerializeField] GameObject plantTaskInfoCollider;
+    [SerializeField] float time = 3f;
+
+    public void Start()
+    {
+        if (waterPlantTaskText.IsActive())
+        {
+            plantTaskInfoCollider.SetActive(true);
+        }
+    }
+    public void Interact()
+    {
+        waterPlantTaskText.text = "<s> - " + EndTaskMessage + "</s>";
+        StartCoroutine(destroyOnTime());
+    }
+
+    IEnumerator destroyOnTime()
+    {
+        yield return new WaitForSeconds(time);
+        waterPlantTaskText.enabled = false;
+    }
+}
