ce4499fc13c5c02893d29c3bd4e66de2366ee3b2
Reumatyzm <jaroszewskakaja@gmail.com>
2024-05-19 23:19:27 +0200

    #20.1
    Half way to havin end day in office

--- Drug Office/Assets/Scripts/OfficeScene/EndOfTheLoop.cs
+++ Drug Office/Assets/Scripts/OfficeScene/EndOfTheLoop.cs
@@ -5,35 +5,49 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class EndOfTheLoop : MonoBehaviour, iInteractable
+public class EndOfTheLoop : MonoBehaviour
 {
-    public List<GameObject> taskTexts;
+    public List<TextMeshProUGUI> taskTexts = new List<TextMeshProUGUI>();
     public GameObject endDayText;
-    public elevatorOpen elevatorOpenScript;
+    public Animator elevatorAnim1;
+    public Animator elevatorAnim2;
+    private string elevatorDoorOpen1 = "elevatorDoorOpen";
+    private string elevatorDoorOpen2 = "elevatorDoorOpen2";
 
-
-    public void Interact()
+    public void Start()
     {
-        SceneManager.LoadScene(0);
+        taskTexts.Add(GameObject.Find("PapersTaskText").GetComponent<TextMeshProUGUI>());
+        taskTexts.Add(GameObject.Find("CoffeeTaskText").GetComponent<TextMeshProUGUI>());
+        taskTexts.Add(GameObject.Find("PlantTaskText").GetComponent<TextMeshProUGUI>());
+        taskTexts.Add(GameObject.Find("MailTaskText").GetComponent<TextMeshProUGUI>());
     }
 
     public void Update()
     {
         bool isDayEnded = true;
-        foreach (GameObject tTexts in taskTexts)
+        foreach (TextMeshProUGUI tTexts in taskTexts)
         {
-            if (tTexts.activeSelf)
+            if (tTexts.enabled)
             {
                 Debug.Log("foreach");
                 isDayEnded = false;
                 break;
             }
+
         }
 
-        if(isDayEnded)
+        if (isDayEnded)
         {
-            elevatorOpenScript.enabled = true;
             endDayText.SetActive(true);
+            StartCoroutine(openingElevatorAfterDelay());
         }
+
+    }
+
+    IEnumerator openingElevatorAfterDelay(float time = 1f)
+    {
+        yield return new WaitForSeconds(time);
+        elevatorAnim1.Play(elevatorDoorOpen1, 0, 0f);
+        elevatorAnim2.Play(elevatorDoorOpen2, 0, 0f);
     }
 }
--- Drug Office/Assets/Scripts/OfficeScene/Tasks/PlantWaterTask.cs
+++ Drug Office/Assets/Scripts/OfficeScene/Tasks/PlantWaterTask.cs
@@ -5,27 +5,20 @@ using TMPro;
 
 public class PlantWaterTask : MonoBehaviour, iInteractable
 {
-    [SerializeField] GameObject waterPlantTaskText;
+    [SerializeField] TextMeshProUGUI waterPlantTaskText;
     [SerializeField] string EndTaskMessage = "*sounds of draining water*";
     [SerializeField] GameObject plantTaskInfoCollider;
     [SerializeField] float time = 3f;
 
-    public void Start()
-    {
-        if (waterPlantTaskText.activeSelf)
-        {
-            plantTaskInfoCollider.SetActive(true);
-        }
-    }
     public void Interact()
     {
-        //waterPlantTaskText. = "<s> - " + EndTaskMessage + "</s>";
+        waterPlantTaskText.text = "<s> - " + EndTaskMessage + "</s>";
         StartCoroutine(destroyOnTime());
     }
 
     IEnumerator destroyOnTime()
     {
         yield return new WaitForSeconds(time);
-        waterPlantTaskText.SetActive(false);
+        waterPlantTaskText.enabled = false;
     }
 }
