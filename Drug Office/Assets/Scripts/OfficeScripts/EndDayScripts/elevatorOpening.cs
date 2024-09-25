5cc1761365a62ba18ea54a7345c9c3ec855e187f
Reumatyzm <jaroszewskakaja@gmail.com>
2024-09-25 22:07:00 +0200

    #24.2
    Minor (mainly cosmetic) changes

--- Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/elevatorOpening.cs
+++ Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/elevatorOpening.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class elevatorOpening : MonoBehaviour
 {
-    public Animator elevatorAnim1;
-    public Animator elevatorAnim2;
+    [SerializeField] Animator elevatorAnim1;
+    [SerializeField] Animator elevatorAnim2;
     private string elevatorDoorOpen1 = "elevatorDoorOpenAnim";
     private string elevatorDoorOpen2 = "elevatorDoorOpenAnim1";
     public bool isElevatorDoorOpened = false;
-    public float time = 3f;
+    private float time = 3f;
 
     public void Start()
     {
