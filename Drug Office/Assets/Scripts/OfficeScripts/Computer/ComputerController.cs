a37a51fa07404f98f6d7388157be9a9fcd14cbc7
ALEKSANDERK1 <68990212+ALEKSANDERK1@users.noreply.github.com>
2024-10-06 22:24:02 +0200

    Computer 1/2
    Komputer z starymi grafikami, działa częściowo (zostało trochę kodu z debug)

--- Drug Office/Assets/Scripts/OfficeScripts/Computer/ComputerController.cs
+++ Drug Office/Assets/Scripts/OfficeScripts/Computer/ComputerController.cs
@@ -1,119 +1,40 @@
 using UnityEngine;
-using UnityEngine.UI; // For UI elements like buttons
-using TMPro; // For TextMeshPro elements
-using System.Collections.Generic;
-using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class ComputerScreenController : MonoBehaviour
 {
-    public GameObject computerScreen; // The "ComputerScreenPanel"
-    public TMP_Text emailContentText; // The "EmailContentText" TextMeshPro element
-    public TMP_Text emailResponse1Text;
-    public TMP_Text emailResponse2Text;
-
-    
-    public Button emailResponse1;
-    public Button emailResponse2;
-    public GameObject emailScrollView; // The "EmailScrollView"
-    public Button closeComputerButton; // The "CloseButton" as a standard Button, not TMP_Button
-
+    public GameObject computerScreen; 
+    public TMP_Text emailContentDisplay; // For displaying the email content
+    public Button closeComputerButton; 
     public GameObject emailContent;
-    public GameObject InstructionsText;
-
     public Button back;
+
     char Response = 'Z';
 
     [SerializeField] TextMeshProUGUI mailTaskText;
     [SerializeField] string taskDoneMessage = "DONE!";
 
-
     void Start()
     {
-        //computerScreen.SetActive(false); // Hide on start
         closeComputerButton.onClick.AddListener(CloseComputerScreen);
         back.onClick.AddListener(GoBack);
-        emailResponse1.onClick.AddListener(EmailResponse1act);
-        emailResponse2.onClick.AddListener(EmailResponse2act);
     }
 
-// Add this method to your ComputerScreenController.cs script
-
-    public TMP_Text emailContentDisplay; // Assign in inspector, use a TextMeshPro - Text component from your UI
-
-    public global::System.Char Response1 { get => Response; set => Response = value; }
-
-    public void GoBack()
-    {
-        emailScrollView.SetActive(true);
-        emailContent.gameObject.SetActive(false);
-        mailTaskText.text = "<s> - " + taskDoneMessage + "</s>";
-        StartCoroutine(destroyOnTime());
-    }
     public void DisplayEmailContent(string content)
     {
-        emailContentDisplay.text = content; // Display the email content in the UI
-        
-        // Show the content area (assuming emailContentDisplay's gameObject is the container for the content)
-        emailContentDisplay.gameObject.SetActive(true);
-
-        // Hide the email list ScrollView
-        emailScrollView.SetActive(false);
+        emailContentDisplay.text = content;
         emailContent.gameObject.SetActive(true);
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            CloseComputerScreen();
-        }
-    }
-
-    public void ToggleComputerScreen()
-    {
-        computerScreen.SetActive(!computerScreen.activeSelf);
     }
 
     void CloseComputerScreen()
     {
         computerScreen.SetActive(false);
-        InstructionsText.SetActive(true);
-        FirstPersonCamera.mouseSesnsitivity = 1000f;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-    public void ToggleEmailListDisplay(bool show)
-    {
-        // This line shows or hides the email ScrollView based on the passed boolean
-        emailScrollView.SetActive(show);
-
-        // Optionally, when showing the email list, you might want to hide the email content display.
-        if(show)
-        {
-            emailContentDisplay.gameObject.SetActive(false);
-        }
     }
 
-    void EmailResponse1act()
+    public void GoBack()
     {
-        Response = 'A';
-        SendEmailResponse();
-    }
-    void EmailResponse2act()
-    {
-        Response = 'B';
-        SendEmailResponse();
-    }
-    void SendEmailResponse()
-    {
-        Debug.Log("Email response sent: " + Response1);
-        GoBack();
-        CloseComputerScreen();
-
-    }
-
-    IEnumerator destroyOnTime(float time = 3f)
-    {
-        yield return new WaitForSeconds(time);
-        mailTaskText.enabled = false;
+        emailContent.gameObject.SetActive(false);
+        mailTaskText.text = "<s> - " + taskDoneMessage + "</s>";
     }
 }
--- Drug Office/Assets/Scripts/OfficeScripts/Computer/DisplayEmailContent.cs
+++ Drug Office/Assets/Scripts/OfficeScripts/Computer/DisplayEmailContent.cs
@@ -5,49 +5,179 @@ using System.Collections.Generic;
 
 public class EmailListController : MonoBehaviour
 {
-    public GameObject emailItemPrefab;
-    public Transform emailListContentTransform;
+    public List<Button> emailButtons = new List<Button>(); // Your 7 email buttons
+    public Button nextButton; // Button to reveal the next email button
     public ComputerScreenController computerScreenController;
 
     private List<string> emails = new List<string>();
+    private int currentEmailIndex = 0; // Tracks the current email being shown
 
     void Start()
     {
-        AddEmail("Subject: Welcome\nDear User, Welcome to our service!");
-        AddEmail("Subject: Update\nWe have updated our terms of service.");
-        AddEmail("Subject: Newsletter\nCheck out our latest news in this newsletter.");
+        Debug.Log("Start method called in EmailListController");
+
+        // Adding email content
+        AddEmail("Subject: Welcome\n(1)Dear User, Welcome to our service!");
+        AddEmail("Subject: Update\n(2)We have updated our terms of service.");
+        AddEmail("Subject: Newsletter\n(3)Check out our latest news in this newsletter.");
+        AddEmail("Subject: Welcome\n(4)Dear User, Welcome to our service!");
+        AddEmail("Subject: Update\n(5)We have updated our terms of service.");
+        AddEmail("Subject: Newsletter\n(6)Check out our latest news in this newsletter.");
+
+        Debug.Log("Emails added, total: " + emails.Count);
+
+        // Populate email buttons and check if they are working
+        PopulateEmailButtons();
         
-        PopulateEmailList();
-    }
+        Debug.Log("Email buttons populated");
 
-    void PopulateEmailList()
-    {
-        foreach (string email in emails)
+        // Initially hide all mail buttons except the first one
+        for (int i = 0; i < emailButtons.Count; i++)
         {
-            GameObject emailItem = Instantiate(emailItemPrefab, emailListContentTransform);
-            TMP_Text emailText = emailItem.GetComponentInChildren<TMP_Text>();
-            Button emailButton = emailItem.GetComponent<Button>();
-
-            if (emailText != null && emailButton != null)
+            if (i == 0)
             {
-                emailText.text = email;
-                string localEmail = email; // Local copy to avoid capturing loop variable
-                emailButton.onClick.AddListener(() => OpenEmail(localEmail));
+                emailButtons[i].gameObject.SetActive(true); // Show first email button
             }
+            else
+            {
+                emailButtons[i].gameObject.SetActive(false); // Hide others
+            }
+        }
 
+        Debug.Log("First email button visible, others hidden");
+
+        // Add listener for the Next button to reveal the next email
+        if (nextButton != null)
+        {
+            nextButton.onClick.AddListener(ShowNextEmailButton);
+            Debug.Log("Next button listener added");
+        }
+        else
+        {
+            Debug.LogError("Next button is null, check assignment in Inspector");
         }
     }
 
-    void OpenEmail(string emailContent)
+
+    void PopulateEmailButtons()
     {
-        // Assuming you have a method in ComputerScreenController to display email content
-        computerScreenController.DisplayEmailContent(emailContent);
-        computerScreenController.ToggleEmailListDisplay(false);
+        Debug.Log("Populating email buttons");
+
+        for (int i = 0; i < emailButtons.Count; i++)
+        {
+            if (emailButtons[i] == null)
+            {
+                Debug.LogError("Email button at index " + i + " is null!");
+                continue;
+            }
+
+            int index = i; // Local copy to avoid closure issues
+
+            if (i < emails.Count)
+            {
+                // We don't need to set the text since the buttons are photos, but we still need to attach the listener
+                Debug.Log("Setting up email button " + i + " for email: " + emails[i]);
+
+                emailButtons[i].onClick.AddListener(() => OpenEmail(emails[index], emailButtons[index]));
+            }
+            else
+            {
+                Debug.LogError("Not enough emails for button " + i);
+            }
+        }
     }
 
-    // Method to add emails to the list (for testing)
+
+
+
+    void OpenEmail(string emailContent, Button emailButton)
+    {
+        Debug.Log("Opening email: " + emailContent);
+
+        // Display the email content using ComputerScreenController
+        if (computerScreenController != null)
+        {
+            computerScreenController.DisplayEmailContent(emailContent);
+        }
+        else
+        {
+            Debug.LogError("ComputerScreenController is not assigned.");
+        }
+
+        // Hide the button after the email is opened
+        emailButton.gameObject.SetActive(false);
+        Debug.Log("Email button hidden after opening.");
+
+        // Show the next email button, if there is one
+        ShowNextEmailButton();
+
+        // Check if all emails have been answered
+        CheckIfAllEmailsAnswered();
+    }
+
+    void ShowNextEmailButton()
+    {
+        Debug.Log("Next button clicked");
+
+        // Reveal the next hidden email button, if there is one
+        if (currentEmailIndex < emailButtons.Count - 1)
+        {
+            currentEmailIndex++; // Move to the next email
+            Debug.Log("Current email index: " + currentEmailIndex);
+
+            emailButtons[currentEmailIndex].gameObject.SetActive(true); // Show the next email button
+            Debug.Log("Email button " + currentEmailIndex + " is now visible.");
+        }
+        else
+        {
+            Debug.Log("No more emails to show.");
+        }
+    }
+
+    void CheckIfAllEmailsAnswered()
+    {
+        Debug.Log("Checking if all emails are answered");
+
+        bool allAnswered = true;
+
+        foreach (Button btn in emailButtons)
+        {
+            if (btn.gameObject.activeSelf)
+            {
+                allAnswered = false;
+                break;
+            }
+        }
+
+        if (allAnswered)
+        {
+            Debug.Log("All emails have been answered.");
+            LogAllEmailsUsed();
+        }
+    }
+
+    void LogAllEmailsUsed()
+    {
+        Debug.Log("Logging all answered emails:");
+
+        foreach (string email in emails)
+        {
+            Debug.Log("Answered email: " + email);
+        }
+    }
+
+    // Method to manually add an email (if needed)
     public void AddEmail(string emailContent)
     {
         emails.Add(emailContent);
+        Debug.Log("Email added: " + emailContent);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
