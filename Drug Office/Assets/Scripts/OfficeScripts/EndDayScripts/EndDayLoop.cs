5b3bd6c93187792b09d56efc05faca4b50a7c9c8
Reumatyzm <jaroszewskakaja@gmail.com>
2024-11-03 23:25:51 +0100

    #26.7
    Temporary fix for one problem lol

--- Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/EndDayLoop.cs
+++ Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/EndDayLoop.cs
@@ -8,6 +8,10 @@ public class EndDayLoop : MonoBehaviour
     public List<TextMeshProUGUI> taskTexts = new List<TextMeshProUGUI>();
     public GameObject endDayText;
     public BoxCollider elevatorButtonCollider;
+    [SerializeField] Animator elevatorAnim1;
+    [SerializeField] Animator elevatorAnim2;
+    private string elevatorDoorOpen1 = "elevatorDoorOpenAnim";
+    private string elevatorDoorOpen2 = "elevatorDoorOpenAnim1";
 
     public void Update()
     {
@@ -24,6 +28,9 @@ public class EndDayLoop : MonoBehaviour
 
         if (isDayEnded)
         {
+            Debug.Log("suck");
+            elevatorAnim1.CrossFade(elevatorDoorOpen1, 0f);
+            elevatorAnim2.CrossFade(elevatorDoorOpen2, 0f);
             elevatorButtonCollider.enabled = true;
             endDayText.SetActive(true);
         }
