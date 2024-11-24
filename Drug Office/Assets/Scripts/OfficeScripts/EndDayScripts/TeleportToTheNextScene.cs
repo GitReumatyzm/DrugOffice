4d3d5fac4e47a48f389b1e89d0358c9ad920c7f0
ALEKSANDERK1 <68990212+ALEKSANDERK1@users.noreply.github.com>
2024-11-24 17:44:31 +0100

    Day swithcher 1/2

--- Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/TeleportToTheNextScene.cs
+++ Drug Office/Assets/Scripts/OfficeScripts/EndDayScripts/TeleportToTheNextScene.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class TeleportToTheNextScene : MonoBehaviour, iInteractable
 {
     public Animator elevatorAnim1;
+    public DaySwitcher daySwitcher;
     public Animator elevatorAnim2;
     private string elevatorDoorClose1 = "elevatorDoorCloseAnim";
     private string elevatorDoorClose2 = "elevatorDoorCloseAnim1";
@@ -28,6 +29,8 @@ public class TeleportToTheNextScene : MonoBehaviour, iInteractable
         SceneManager.LoadScene(0);
         player.SetActive(false);
         dullPlayer.SetActive(true);
+        daySwitcher.SwitchDayTo2();
+
 
     }
 }
